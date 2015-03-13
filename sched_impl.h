4b87b43aa4e6664da8ba60833cb15f264f8b7d1c
rob <rkelly@nmt.edu>
2015-03-13 03:40:51 -0600

    Implemented queue operations. Also synchronization. Sorry, it just kinda happened.

--- sched_impl.h
+++ sched_impl.h
@@ -1,12 +1,26 @@
 #ifndef	__SCHED_IMPL__H__
 #define	__SCHED_IMPL__H__
 
+#include <semaphore.h>
+#include <pthread.h>
+
+#include "list.h"
+
 struct thread_info {
-	/*...Fill this in...*/
+  /* Scheduling queue */
+  sched_queue_t* queue;
+  list_elem_t* queue_elem;
 };
 
 struct sched_queue {
-	/*...Fill this in...*/
+  /* Actual data structure */
+  list_t* list;
+
+  /* Semaphores to synchronize queue capacity */
+  sem_t* production, * consumption;
+
+  /* Mutex lock to synchronize actual read/write operations on queue */
+  pthread_mutex_t* access_mutex;
 };
 
 #endif /* __SCHED_IMPL__H__ */
