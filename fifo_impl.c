f244cf54742ff6862439fb20b97a8ec0bdc7de05
rob <rkelly@nmt.edu>
2015-03-13 21:54:04 -0600

    Implemented fifo-policy wait method

--- fifo_impl.c
+++ fifo_impl.c
@@ -5,8 +5,25 @@
  * scheduling.
  */
 
+#include <stdio.h>
+
 #include "fifo_impl.h"
+#include "list.h"
+#include "sched_impl.h"
 
 void fifo_wait(sched_queue_t *queue) {
-  // TODO
+  /* Get current worker from head of queue */
+  list_elem_t* head = list_get_head(queue->list);
+  if(!head) {
+    /* No workers active, so... Just return */
+    return;
+  }
+
+  thread_info_t* worker = (thread_info_t*) head->datum;
+
+  /* Block until worker has finished */
+  if(pthread_mutex_lock(worker->yield_cpu)) {
+    /* Handle error locking cpu yield */
+    perror("scheduling policy waiting on worker");
+  }
 }
